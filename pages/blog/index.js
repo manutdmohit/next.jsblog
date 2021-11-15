815fd55b9ef28d0a6bb61e3a350043680a9c99a9
manutdmohit <saudmohit@gmail.com>
2021-11-15 10:19:20 +0545

    start pagination and generate paths

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,46 +1,6 @@
-import fs from 'fs';
-import path from 'path';
-import Link from 'next/link';
-import matter from 'gray-matter';
-import Layout from '../../components/Layout';
-import Post from '../../components/Post';
+import { getStaticProps } from './page/[page_index]';
 
-import { sortByDate } from '../../utils';
+import BlogPage from './page/[page_index]';
 
-export async function getStaticProps() {
-  const files = fs.readdirSync(path.join('posts'));
-
-  const posts = files.map((filename) => {
-    const slug = filename.replace('.md', '');
-
-    const markdownWithMeta = fs.readFileSync(
-      path.join('posts', filename),
-      'utf-8'
-    );
-
-    const { data: frontmatter } = matter(markdownWithMeta);
-
-    return {
-      slug,
-      frontmatter,
-    };
-  });
-
-  return {
-    props: { posts: posts.sort(sortByDate)},
-  };
-}
-
-export default function Blog({ posts }) {
-  return (
-    <Layout>
-      <h1 className="text-5xl border-b-4 p-5 font-bold">Blog</h1>
-
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
-      </div>
-    </Layout>
-  );
-}
+export { getStaticProps };
+export default BlogPage;
