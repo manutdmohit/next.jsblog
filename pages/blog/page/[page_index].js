f2a26be2eeba22220df6ef6c2e93edeaed4ead20
manutdmohit <saudmohit@gmail.com>
2021-11-16 10:46:43 +0545

    clean up getposts

--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -1,13 +1,12 @@
 import fs from 'fs';
 import path from 'path';
-import matter from 'gray-matter';
 
-import Layout from '../../../components/Layout';
-import Post from '../../../components/Post';
-import { sortByDate } from '../../../utils';
+import Layout from '@/components/Layout';
+import Post from '@/components/Post';
 
-import { POSTS_PER_PAGE } from '../../../config';
-import Pagination from '../../../components/Pagination';
+import { POSTS_PER_PAGE } from '@/config/index';
+import Pagination from '@/components/Pagination';
+import getPosts from '@/lib/posts';
 
 export async function getStaticPaths() {
   const files = fs.readdirSync(path.join('posts'));
@@ -33,27 +32,14 @@ export async function getStaticProps({ params }) {
 
   const files = fs.readdirSync(path.join('posts'));
 
-  const posts = files.map((filename) => {
-    const slug = filename.replace('.md', '');
-
-    const markdownWithMeta = fs.readFileSync(
-      path.join('posts', filename),
-      'utf-8'
-    );
-
-    const { data: frontmatter } = matter(markdownWithMeta);
-
-    return {
-      slug,
-      frontmatter,
-    };
-  });
+  const posts = getPosts();
 
   const numPages = Math.ceil(files.length / POSTS_PER_PAGE);
   const pageIndex = page - 1;
-  const orderPosts = posts
-    .sort(sortByDate)
-    .slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE);
+  const orderPosts = posts.slice(
+    pageIndex * POSTS_PER_PAGE,
+    (pageIndex + 1) * POSTS_PER_PAGE
+  );
 
   return {
     props: {
